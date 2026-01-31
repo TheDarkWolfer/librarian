ebc0975be21c936639703e62a26899711037f716
Camille PRADO <camille.prado@felisse.fr>
2026-01-31 01:03:34 +0100

    You know what ? It's past my bedtime, I remembered my page may show up bigots, and I won't let that happend. Now if you search up JK Rowling's works, you get a nice message telling you how awful she is. Made me work with conditional rendering and more UI elements, so that's a plus (˶ˆᗜˆ˵) Might expand the net to catch more bigots later, and I know it's not "politically correct" or anything, but this woman is part of why I am scared of going out alone, even with a **moon-forsaken** *SEA* between the two of us, and she sees our community as pests even though we're all trying to live without bothering anyone, so : ***SIGNALED TRANSPHOBIA IN SEARCH RESULTS - EXPERIMENTAL FEATURE. MORE AUTHORS MAY GET THE PURPLE BOX OF SHAME***

---
+++ src/assets/activism.tsx
@@ -0,0 +1,163 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+// Fun fact ! JK Rowling est la raison pour laquelle je ne peux pas mettre les pieds en Angleterre sans risquer de me faire, et je déconne à peine, agresser sexuellement par un policier, juste pour le lol. De plus, cette femme contribue ouvertement à notre génocide, et ses "actions" pour "protéger" les femmes ont déjà causé préjudice à beacoup de femmes, y compris des femmes comme moi, qui avaient eu le malheur de ne pas sembler assez féminines à la mauvaise personne. Donc, si quelqu'un s'intéresse à elle à travers mon site, qui n'est pas tenu à une quelconque neutralité politique à ma connaissance, je vais pas me gêner pour faire ma partie du boulot
+function checkAuthor(AuthorName:string):boolean {
+  const normalizedInput = AuthorName
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '');
+
+  // Permutations de "J.K. Rowling"
+  const sheWhoMustNotBeNamed = [
+    'jkrowling',
+    'rowlingjk',
+    'j.k.rowling',
+    'j k rowling',
+    'rowlingj.k.',
+    'joannekrowling',
+    'jrowling',
+    'j.k.rolling',
+    'j.k.roling',
+  ];
+
+  return sheWhoMustNotBeNamed.includes(normalizedInput);
+}
+
+export function rmTerf(AuthorName:string) {
+  if (!checkAuthor(AuthorName)) { return null ; }
+    // https://www.ms.now/opinion/msnbc-opinion/jk-rowling-uk-trans-women-ruling-rcna201947
+    // https://en.wikipedia.org/wiki/Political_views_of_J._K._Rowling // just look at the second fucking paragraph
+    // https://en.wikipedia.org/wiki/Political_views_of_J._K._Rowling#Transgender_people
+    // https://theweek.com/feature/1020838/jk-rowlings-transphobia-controversy-a-complete-timeline
+    // https://www.them.us/story/timeline-jk-rowlings-descent-into-transphobia
+  return (
+    <Box
+      className="doingMyPart"
+      sx={{
+	color:"#c4a7e7",
+	backgroundColor:"#6e6a86",
+	borderRadius:"1rem",
+	padding:"0.5rem",
+	margin:"0.5rem",
+      }}>
+      <Typography
+	variant="body1" 
+	fontWeight="bold"
+	>
+      Cette autrice est extrèmement transphobe et homophobe par ses prises d'actions. Elle est l'une des plus grandes raisons la transformation de l'Angleterre en ce que certain.es d'entre nous appellent "Terf Island" : "l'Île Anti-Trans", si on traduisait l'idée. Elle contribue à la haine envers nombreux.ses membres de la communauté queer, et ce faisant, je refuse de ne pas vous demander de réfléchir aux actions de cette autrice, et de vous conseiller des organismes plus corrects tels que le Trevor Project, qui aide les jeunes personnes queer, spécifiquement trans, à reçevoir l'assistance dont iels ont besoin, Rainbow Railroad, permettant aux personnes queer de fuir les pays qui les persécutent, ou bien des auteur.ices comme Jammie Dodger, ou Lexie Agresti.
+	</Typography>
+      <Box sx={{
+	borderColor:"#9ccfd8",
+      }}>
+	<Box sx={{ padding:"0.3rem", display: "flex", gap: 1, justifyContent: "center" }}>
+	   <Button
+	    sx={{
+	      color:"9ccfd8",
+	      backgroundColor:"#3e8fb0",
+	    }}
+	    variant="outlined"
+	    size="small"
+	    href="https://www.thetrevorproject.org/"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    The Trevor Project
+	  </Button>     
+	  <Button
+	    sx={{
+	      color:"9ccfd8",
+	      backgroundColor:"#3e8fb0",
+	    }}variant="outlined"
+	    size="small"
+	    href="https://www.rainbowrailroad.org/"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    Rainbow Railroad
+	  </Button>
+      </Box>
+      <Box sx={{ padding:"0.3rem", display: "flex", gap: 1, justifyContent: "center" }}>
+	  <Button
+	    sx={{
+	      backgroundColor:"#eb6f92",
+	      color:"#e0def4"
+	    }}
+	    variant="outlined"
+	    size="small"
+	    href="https://www.youtube.com/@Jammidodger"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    <YouTubeIcon/> Jamie Dodger
+	  </Button>
+	  <Button
+	    sx={{
+	      backgroundColor:"#ea9a97",
+	      color:"#e0def4"
+	    }}
+	    variant="outlined"
+	    size="small"
+	    href="https://www.instagram.com/aggressively_trans/"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    <InstagramIcon/> Lexie Agresti
+	  </Button>
+	</Box>
+
+	<Box sx={{ padding:"0.3rem", display: "flex", gap: 1, justifyContent: "center" }}>
+	  <Button
+	    variant="outlined"
+	    size="small"
+	    href="https://www.ms.now/opinion/msnbc-opinion/jk-rowling-uk-trans-women-ruling-rcna201947"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    AND
+	  </Button>
+	  <Button
+	    variant="outlined"
+	    size="small"
+	    href="https://en.wikipedia.org/wiki/Political_views_of_J._K._Rowling"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    I
+	  </Button>
+	  <Button
+	    variant="outlined"
+	    size="small"
+	    href="https://en.wikipedia.org/wiki/Political_views_of_J._K._Rowling#Transgender_people"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    CITE
+	  </Button>
+	  <Button
+	    variant="outlined"
+	    size="small"
+	    href="https://theweek.com/feature/1020838/jk-rowlings-transphobia-controversy-a-complete-timeline"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    MY
+	  </Button>
+	  <Button
+	    variant="outlined"
+	    size="small"
+	    href="https://www.them.us/story/timeline-jk-rowlings-descent-into-transphobia"
+	    target="_blank"
+	    rel="noopener noreferrer"
+	  >
+	    SOURCES
+	  </Button>
+	</Box>
+      </Box>
+    </Box>
+  )
+}
+
--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -1,17 +1,21 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 import { useAuthorSearch } from '../api_logic/Requests.tsx';
+
+import { rmTerf } from '../assets/activism.tsx';
 
 export function AuthorDetails({ authorKey }: { authorKey: string }) {
   const { data, loading, error } = useAuthorSearch(authorKey);
 
   const theme = useTheme();
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  if (loading) return <Typography>Loading...</Typography>;
+  if (error) return <Typography>Error: {error}</Typography>;
   if (!data) return null;
 
-  // Bio can be string or object
+  // On fait ce qu'on peut avec la biographie...
   const bio = typeof data.bio === 'string' ? data.bio : data.bio?.value;
 
   // Photo URL
@@ -19,21 +23,25 @@ export function AuthorDetails({ authorKey }: { authorKey: string }) {
     ? `https://covers.openlibrary.org/a/id/${data.photos[0]}-M.jpg` 
     : null;
 
+
+    const yeetTerf = rmTerf(data.name);
+
   return (
     <Box sx={{
       width:"50rem",
       margin:"0 auto",
       textAlign:"center",
       paddingBottom:"10rem",
-      backgroundColor:theme.palette.secondary.main,
+      backgroundColor:theme.palette.background.paper,
       padding:"1rem",
       borderRadius:"1rem",
     }}>
       {photo && <img src={photo} alt={data.name} />}
       <h3>{data.name}</h3>
-      {data.birth_date && <p>Born: {data.birth_date}</p>}
-      {data.death_date && <p>Died: {data.death_date}</p>}
-      {bio && <p>{bio}</p>}
+      {yeetTerf && yeetTerf}
+      {data.birth_date && <Typography>Born: {data.birth_date}</Typography>}
+      {data.death_date && <Typography>Died: {data.death_date}</Typography>}
+      {bio && <Typography>{bio}</Typography>}
     </Box>
   );
 }
