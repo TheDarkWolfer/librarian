19b5b693f76e90e6255f087e0b31416a7ee03466
Camille PRADO <camille.prado@felisse.fr>
2026-02-07 16:07:26 +0100

    (つ╥﹏╥)つ

--- src/assets/activism.tsx
+++ src/assets/activism.tsx
@@ -28,7 +28,7 @@ function checkAuthor(AuthorName:string):boolean {
 }
 
 export function rmTerf(AuthorName:string) {
-  if (import.meta.env.VITE_ENABLE_ACTIVISM === false) return null; // killswitch
+  if (import.meta.env.VITE_ENABLE_ACTIVISM == 'false') return null; // killswitch
   if (!checkAuthor(AuthorName)) { return null ; }
     // https://www.ms.now/opinion/msnbc-opinion/jk-rowling-uk-trans-women-ruling-rcna201947
     // https://en.wikipedia.org/wiki/Political_views_of_J._K._Rowling // just look at the second fucking paragraph
